988f954db7bb53481c70b947ac497b91be2d2bfe
Delores VanHoose <lvanhoose5@gmail.com>
2022-03-27 15:26:14 -0500

    Search logic

---
+++ scripts/searchscript.js
@@ -0,0 +1,21 @@
+function search(){
+    /*
+    // Stores posts in order of most relevant
+    const searchPosts = [];
+    var postTitle = "";
+    var postSummary = "";
+    
+    // Search for key words in post, those that uses keywords most is higher up on list
+    for (let i = 0; i < posts.length; i++) {
+        postTitle = posts[i][0];
+        postSummary = posts[i][1];
+        
+        // Checks each word in title
+        
+        
+        // Checks each word in summary
+        
+    }
+    
+    */
+}
