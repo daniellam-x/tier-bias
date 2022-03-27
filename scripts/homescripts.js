280882f39a66edaf866a6a329b20a2ead041c8df
Delores VanHoose <lvanhoose5@gmail.com>
2022-03-27 14:30:33 -0500

    Working on sorting logic

---
+++ scripts/homescripts.js
@@ -0,0 +1,20 @@
+const currentPosts = [];
+const controversialSort = [];
+const newestSort = [];
+const newPosts = [];
+
+function controversialSort(){
+    // Sort current posts
+    
+}
+
+function addControversial(){
+    
+}
+
+function newestSort(){
+}
+
+function addNewest(){
+    
+}
